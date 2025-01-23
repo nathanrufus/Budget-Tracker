e025d2617878647ec0428c96ca3f4aeb4a45ffc6
kibet nathan <nathanrufus540@gmail.com>
2025-01-23 11:34:38 +0300

    creating components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import Services from "./component/Services";
+import Testimonials from "./component/Testimonials";
+import Access from "./component/Access";
+import Contact from "./component/Contact";
+import Footer from "./component/Footer";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Services/>
+    <Testimonials/>
+    <Access/>
+    <Contact/>
+    <Footer/>
+    </>
+  );
 }
 
 export default App;
