29a2f2ee3f13319a2a8daa0fc48ec21dca883288
kibet nathan <nathanrufus540@gmail.com>
2025-01-24 16:31:04 +0300

    finished the services component and fixed some bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,9 +11,9 @@ function App() {
   return (
     <>
     <Navbar/>
-    {/* <Home/>
+    <Home/>
     <Services/>
-    <Testimonials/>
+    {/* <Testimonials/>
     <Access/>
     <Contact/>
     <Footer/> */}
