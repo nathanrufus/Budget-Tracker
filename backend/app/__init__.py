cc771ecef713c3fc583b0fad95da9f2f6c786e26
kibet nathan <nathanrufus540@gmail.com>
2025-03-16 08:40:00 +0300

    Done minor changes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
 
 
 # import os
@@ -14,6 +16,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     # app.config['SQSignupApiLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///budget_tracker.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -24,36 +27,20 @@ def create_app():
     migrate.init_app(app,db)
 
     from app.auth import auth_bp
-    from app.expenses import expenses_bp
+    # from app.expenses import expenses_bp
     from app.budgets import budgets_bp
     from app.goals import goals_bp
-<<<<<<< HEAD
-    from app.notification import notifications_bp
+    # from app.notification import notifications_bp
     from app.expense import expense_bp
-    from app.budgets import budgets_bp 
 
-=======
-    from app.reports import reports_bp
-    from app.notifications import notifications_bp
-    from app.monthly_summaries import monthly_summaries_bp
-    
-    # Register other blueprints as needed
->>>>>>> cc82d1b9921790cdbf576e7fc604229b2e6c0ca7
     app.register_blueprint(auth_bp, url_prefix='/auth')
-    app.register_blueprint(expenses_bp, url_prefix='/api')
+    # app.register_blueprint(expenses_bp, url_prefix='/api')
     app.register_blueprint(budgets_bp, url_prefix='/api')
     app.register_blueprint(goals_bp, url_prefix='/api')
-    app.register_blueprint(reports_bp, url_prefix='/api')
-    app.register_blueprint(notifications_bp, url_prefix='/api')
-<<<<<<< HEAD
+    # app.register_blueprint(reports_bp, url_prefix='/api')
+    # app.register_blueprint(notifications_bp, url_prefix='/api')
     app.register_blueprint(expense_bp, url_prefix='/api')
-    app.register_blueprint(budgets_bp, url_prefix='/api')
     
-=======
-    app.register_blueprint(monthly_summaries_bp, url_prefix='/api')
-
-
->>>>>>> cc82d1b9921790cdbf576e7fc604229b2e6c0ca7
 
     return app
 
--- frontend/src/component/Signup.jsx
+++ frontend/src/component/Signup.jsx
@@ -1,17 +1,48 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+
 
 function Signup() {
+
+const navigate=useNavigate()
+
+const[email ,setemail]=useState("") 
+const[password ,setpassword]=useState("")
+const handleSignup = async (e) =>{
+  e.preventDefault()
+  try {
+    const response= await fetch("http://localhost:5000/auth/signup" ,{
+      method: "POST",
+      headers:{"Content-Type": "application/json"},
+      body:JSON.stringify({email ,password})
+    })
+    const data= await response.json()
+    if(response.ok){
+      setemail("")
+      setpassword("")
+      alert("Signup successfully")
+      navigate("/signin")
+   
+    }
+    throw new Error(data.message || "signup failed")
+  } catch (error) {
+    throw error
+  }
+}
+
   return (
     <div className=" mt-20 min-h-screen flex items-center justify-center bg-white">
-     <div className=" w-full max-w-sm bg-gray-100 shadow-lg p-8">
-      <h1 className=" text-2xl font-bold text-center mb-4 text-purple-600"> Sign Up</h1>
-      <form>
-        <input placeholder="email" type="email" required className=" w-full p-3 border rounded mb-4 focus:outline-none"/>
-        <input placeholder="password" type="password" required className="w-full p-3 border rounded mb-4 focus:outline-none "/>
-        <button className=" w-full py-3 bg-purple-600 text-white cursor-pointer rounded hover:bg-blue-600 transition-all duration-700" type="submit">Sign Up</button>
-      </form>
-      <p className=" mt-4 text-sm">Already have an account ? <a href="/login" className=" text-purple-600">Sign In</a></p>
-     </div>
+      <div className=" w-full max-w-sm bg-gray-100 shadow-lg p-8">
+        <h1 className=" text-2xl font-bold text-center mb-4 text-purple-600"> Sign Up</h1>
+        <form onSubmit={handleSignup}>
+          <input value={email} onChange={(e)=> setemail(e.target.value)} placeholder="email" type="email" required className=" w-full p-3 border rounded mb-4 focus:outline-none" />
+          <input value={password} onChange={(e)=> setpassword(e.target.value)} placeholder="password" type="password" required className="w-full p-3 border rounded mb-4 focus:outline-none " />
+          <button className=" w-full py-3 bg-purple-600 text-white cursor-pointer rounded hover:bg-blue-600 transition-all duration-700" type="submit">Sign Up</button>
+        </form>
+        <p className=" mt-4 text-sm">Already have an account ? <a href="/login" className=" text-purple-600">Sign In</a></p>
+      </div>
     </div>
   );
 }
